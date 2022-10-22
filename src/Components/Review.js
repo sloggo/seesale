3ea671bf8fe5645a0ef41d815a7f4c64be19e0b2
sloggo <70816908+sloggo@users.noreply.github.com>
2022-10-22 18:15:01 +0100

    Finish the rest of review card mobile styling

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -8,6 +8,9 @@ function Review() {
         <TopBarTop></TopBarTop>
         <TopBarBottom></TopBarBottom>
         <Image></Image>
+        <Rating></Rating>
+        <ReviewContent></ReviewContent>
+        <ViewComments></ViewComments>
     </div>
   )
 }
@@ -29,7 +32,44 @@ function TopBarBottom(){
 
 function Image(){
     return(
-        <img src='./images/placeholdercontent.png' className='review-image'></img>
+        <img src='./images/placeholdercontent.png' className='review-image' alt='Review Picture'></img>
+    )
+}
+
+function Rating(){
+    return(
+        <div className='review-rating-container'>
+            <div className='review-rating-user'>
+                <ProfileIcon></ProfileIcon>
+                <h4 className='review-rating-user-name'>User</h4>
+            </div>
+
+            <RatingStars></RatingStars>
+        </div>
+    )
+}
+
+function RatingStars(){
+    return(
+        <div className='review-rating-stars-container'>
+            <img className='review-rating-star' src='./images/star.svg' alt='Star symbol'></img>
+            <img className='review-rating-star' src='./images/star.svg' alt='Star symbol'></img>
+            <img className='review-rating-star' src='./images/star.svg' alt='Star symbol'></img>
+            <img className='review-rating-star' src='./images/star.svg' alt='Star symbol'></img>
+            <img className='review-rating-star' src='./images/star.svg' alt='Star symbol'></img>
+        </div>
+    )
+}
+
+function ReviewContent(){
+    return(
+        <p className='review-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat luctus dui, faucibus pulvinar purus scelerisque vitae. Praesent nulla odio, egestas id dictum sed, mollis ut nisi. Curabitur tellus justo, sollicitudin id pretium at, blandit at ante. Morbi nec nisi magna. Nullam sit amet convallis quam. Pellentesque scelerisque tellus vel rutrum hendrerit. Aliquam non risus et purus tristique porttitor sit amet eleifend metus. Donec porttitor facilisis condimentum. In hac habitasse platea dictumst. Sed non dolor dictum, malesuada libero eget, consectetur mi. Aenean quis ipsum ut felis imperdiet consequat sit amet ac orci. Donec et euismod orci. Integer rutrum lorem in scelerisque feugiat. Sed quis ultricies sapien. Curabitur pharetra non risus vitae ullamcorper.</p>
+    )
+}
+
+function ViewComments(){
+    return(
+        <h2 className='review-view-comments'>View all 7 comments...</h2>
     )
 }
 
