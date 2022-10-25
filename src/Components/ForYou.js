b2cc0c4b326239ffa95c9bf11bd2924a08e58f42
sloggo <70816908+sloggo@users.noreply.github.com>
2022-10-25 20:49:52 +0100

    Add for you header and content styling

--- src/Components/ForYou.js
+++ src/Components/ForYou.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import '../Styles/ForYou.css'
+import { ProfileIcon } from './HomeHeader'
 
 function ForYou() {
   return (
     <div className='for-you-container'>
-        <h1>For You</h1>
+        <Header></Header>
+        <Content></Content>
     </div>
   )
 }
 
+function Header(){
+    return(
+        <h1 className='for-you-header'>For You</h1>
+    )
+}
+
+function Content(){
+    return(
+        <div className='for-you-content'>
+            <BusinessContainer></BusinessContainer>
+        </div>
+    )
+}
+
+function BusinessContainer(){
+    return(
+        <div className='for-you-bus-container'>
+            <ProfileIcon></ProfileIcon>
+            <h2>Business</h2>
+        </div>
+    )
+}
 export default ForYou
