0a75e71a6889961c319f0fa4997cbc656ef75bbe
sloggo <70816908+sloggo@users.noreply.github.com>
2022-10-25 20:30:20 +0100

    Adjust review desktop sizing and add maincontent component

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <>
       <HomeHeader isMobile={isMobile}></HomeHeader>
-      <Review></Review>
+      <MainContent></MainContent>
     </>
   );
 }
---
+++ src/MainContent/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Review from '../Components/Review'
+
+function MainContent() {
+  return (
+    <div className='main-content'>
+        <Review></Review>
+    </div>
+  )
+}
+
+export default MainContent
