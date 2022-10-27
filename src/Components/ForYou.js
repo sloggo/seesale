3a705d7740f73c26459deab395a7b7af0194ed1b
sloggo <70816908+sloggo@users.noreply.github.com>
2022-10-27 09:54:46 +0100

    Adjust sizing and gaps of businesses

--- src/Components/ForYou.js
+++ src/Components/ForYou.js
@@ -22,6 +22,7 @@ function Content(){
     return(
         <div className='for-you-content'>
             <BusinessContainer></BusinessContainer>
+            <BusinessContainer></BusinessContainer>
         </div>
     )
 }
