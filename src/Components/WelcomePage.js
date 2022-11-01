c029f0b34a8d62ed68b0b9c8d514ed03173552e9
sloggo <70816908+sloggo@users.noreply.github.com>
2022-11-01 21:54:50 +0000

    Add more subtitles and start product on welc page

--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -7,6 +7,8 @@ function WelcomePage() {
     <div className='welcome-page-container'>
         <Logo></Logo>
         <SubTitle></SubTitle>
+        <SubTitle2></SubTitle2>
+        <Product></Product>
     </div>
   )
 }
@@ -14,14 +16,28 @@ function WelcomePage() {
 function Logo(){
     return(
         <div className='welcome-page-logo-container'>
-            <motion.h1 className='welcome-page-logo' initial={{x:-200}} animate={{x:0}} transition={{duration:.3}}>SeeSale</motion.h1>
+            <motion.h1 className='welcome-page-logo' initial={{x:-200, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:.3}}>SeeSale</motion.h1>
         </div>
     )
 }
 
 function SubTitle(){
     return(
-        <h3 className='welcome-page-subtitle'>See, Sell and Explore</h3>
+        <motion.h3 className='welcome-page-subtitle' initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:.4, delay: 0.5}}>See, Sell and Explore,</motion.h3>
+    )
+}
+
+function SubTitle2(){
+    return(
+        <motion.h3 className='welcome-page-subtitle2' initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:.4, delay: 0.9}}>Social Media ecommerce made easy</motion.h3>
+    )
+}
+
+function Product(){
+    return(
+        <div className='welcome-page-product-container'>
+            <img className='welcome-page-product' src='./images/shoe.png'></img>
+        </div>
     )
 }
 
